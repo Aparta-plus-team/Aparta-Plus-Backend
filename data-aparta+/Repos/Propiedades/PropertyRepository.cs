7ba247fcde0ebc65c783f468471c1500797527fb
ErickTejada04 <erick1310lol@gmail.com>
2025-01-12 23:15:52 -0400

    feat: more images

--- data-aparta+/Repos/Propiedades/PropertyRepository.cs
+++ data-aparta+/Repos/Propiedades/PropertyRepository.cs
@@ -86,7 +86,7 @@ namespace data_aparta_.Repos.Propiedades
             return true;
         }
 
-        public async Task<FileUploadResponse> UploadPortrait(FileUploadInput fileInput)
+        public async Task<FileUploadResponse> UploadPortrait(FileUploadInput fileInput, string propertyId)
         {
             if (fileInput.File == null)
                 throw new ArgumentException("No se ha proporcionado un archivo.");
@@ -96,10 +96,17 @@ namespace data_aparta_.Repos.Propiedades
                 var input = new FileUploadInput
                 {
                     File = fileInput.File,
-                    Type = "contract" // o el tipo que corresponda
+                    Type = "cover" // o el tipo que corresponda
                 };
 
                 var result = await s3Uploader.UploadFileAsync(input);
+
+                var property = await _context.Propiedads.FirstOrDefaultAsync(p => p.Propiedadid == Guid.Parse(propertyId));
+                if (property == null)
+                    throw new ArgumentException("La propiedad especificada no existe.");
+
+                property.Portadaurl = result.Url;
+                await _context.SaveChangesAsync();
 
                 return new FileUploadResponse { 
                     Key = result.Key,
@@ -110,6 +117,74 @@ namespace data_aparta_.Repos.Propiedades
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        public async Task<FileUploadResponse> UploadImages(FileUploadInput fileInput, string propertyId)
+        {
+            if (fileInput.File == null)
+                throw new ArgumentException("No se ha proporcionado un archivo.");
+
+            try
+            {
+                var input = new FileUploadInput
+                {
+                    File = fileInput.File,
+                    Type = "property-image",
+                    PropertyId = propertyId
+                };
+
+                var result = await s3Uploader.UploadFileAsync(input);
+
+                var propiedad = await _context.Propiedads.FirstOrDefaultAsync(p => p.Propiedadid == Guid.Parse(propertyId));
+                if (propiedad == null)
+                    throw new ArgumentException("La propiedad especificada no existe.");
+
+
+                Imagenespropiedade imagenespropiedade = new Imagenespropiedade
+                {
+                    Imagenespropiedadeid = Guid.NewGuid(),
+                    Propiedadid = Guid.Parse(propertyId),
+                    Url = result.Url,
+                    Fechacreacion = DateOnly.FromDateTime(DateTime.UtcNow),
+                    Estado = true,
+                };
+
+                return new FileUploadResponse
+                {
+                    Key = result.Key,
+                    Url = result.Url
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Imagenespropiedade> DeleteImage(string id)
+        {
+            var imagen = await _context.Imagenespropiedades.FirstOrDefaultAsync(i => i.Imagenespropiedadeid == Guid.Parse(id));
+
+            if (imagen == null || imagen.Estado == false)
+                throw new ArgumentException("La imagen especificada no existe.");
+
+            imagen.Estado = false;
+            await _context.SaveChangesAsync();
+
+            return imagen;
+        }
+
+        public async Task<string> DeleteCover(string id)
+        {
+            var propiedad = await _context.Propiedads.FirstOrDefaultAsync(p => p.Propiedadid == Guid.Parse(id));
+
+            if (propiedad == null || propiedad.Estado == false)
+                throw new ArgumentException("La propiedad especificada no existe.");
+
+            propiedad.Portadaurl = null;
+            await _context.SaveChangesAsync();
+
+            return "Portada eliminada exitosamente.";
         }
 
     }
